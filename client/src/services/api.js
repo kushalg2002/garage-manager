3ee5af59318db0d527064a566dda69f097168250
kushalg2002 <kushal@example.com>
2026-08-08 02:38:24 +0530

    Update frontend API to Render

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://garage-manager-api.onrender.com/api",
 });
 
 export default api;
