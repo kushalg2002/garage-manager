e72ce468796e15f69832a175bed34bea808c1ca3
kushalg2002 <kushal@example.com>
2026-08-08 02:42:13 +0530

    Connect frontend to Render backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://garage-manager-api.onrender.com/api",
+  baseURL: "https://garage-manager-kmr5.onrender.com/api",
 });
 
 export default api;
